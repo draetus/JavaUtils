7c924cef77a0109222fbd8f39e6fd450497b9d43
Mauricio <mauricio.macario@palmsoft.com.br>
2024-01-29 20:40:02 -0300

    Refatoração de forma de randomizaçaõ de strign

--- src/utils/RandomUtils.java
+++ src/utils/RandomUtils.java
@@ -1,8 +1,13 @@
 package utils;
 
-public class RandomUtils {
+import java.util.Random;
+
+public interface RandomUtils {
 	
-	private static String ALPHA_NUMERIC_STRING= "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+	public static String ALPHA_NUMERIC_STRING= "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+	public static final String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	
+	public static final Random random = new Random();
 	
 	public static String randomString(int length) {
 		StringBuilder sb = new StringBuilder(length);
@@ -11,7 +16,7 @@ public class RandomUtils {
   
             // generate a random number between
             // 0 to AlphaNumericString variable length
-            int index = (int)(ALPHA_NUMERIC_STRING.length() * Math.random());
+        	int index = random.nextInt(ALPHA_NUMERIC_STRING.length());
   
             // add Character one by one in end of sb
             sb.append(ALPHA_NUMERIC_STRING
